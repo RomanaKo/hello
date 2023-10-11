98c402daf3da809148a2917891d39d5df69eb4e6
Romana Kordikova <romana.kordikova@zurich.com>
2023-10-11 10:17:50 +0200

    added french text

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
+import sys
+
+def french():
+	print('Bon Jour')
+
 def hello():
 	print('Hello')
 	
 def main():
 	hello()
 	
-if _main_ :
-	hello()
+if __name__ = "__main__" :
+	main()
 	
-print('change')
